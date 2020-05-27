066d68690650f4f9a3c0d7d3887f9bebdfdc55ae
Veteran <pradumnagarg@hotmail.com>
2020-05-28 01:23:53 +0530

    Animations part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,23 +7,19 @@ import { switchMap } from 'rxjs/operators';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment'
 import { trigger, state, style, transition, animate } from '@angular/animations'
+import { visibility, flyInOut } from '../animations/app.animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-      state('shown', style({ 
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('*=>*', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(),
+    visibility()
   ]
 })
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,19 @@ import { Leader } from '../shared/Leader';
 import { DishService } from "../services/dish.service";
 import { PromotionService } from "../services/promotion.service";
 import { LeaderService } from "../services/leader.service";
+import { flyInOut } from '../animations/app.animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class HomeComponent implements OnInit {
 
